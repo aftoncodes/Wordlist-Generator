36fc61762071230983a5842b2b4287fd075f5e79
William Afton <107783820+WilliamAfton-codes@users.noreply.github.com>
2022-11-04 20:26:48 +0000

    Added multithreading

--- wordlistgen.py
+++ wordlistgen.py
@@ -1,26 +1,46 @@
+import threading
 import random
 import time
 
-print("Welcome to my Python wordlist generator! Made with <3 by WilliamAfton-codes")    # Welcome message
-var = 0     # Sets loop variable so that if I2 is 'n' it will end ALL loops
-while var == 0:     # Begins whole loop
-    N = int(input("How long would you like each string to be?: "))      # Asks how long each word will be
-    strings = input("How many strings would you like to generate and add to wordlist.txt?: ")       # Asks how many words to make
-    for i in range(int(strings)):   # Makes the strings and writes them to 'wordlist.txt'
-        ran_str = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=N)) + '\n'
-        fo = open('wordlist.txt', 'a')
+threadnum = 1
+strlength = 10
+strnum = 10
+
+
+def process():
+    global threadnum
+    global strlength
+    global strnum
+    for i in range(strnum):
+        ran_str = ''.join(
+            random.choices('abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=strlength)) + '\n'
         fo.seek(0)
         fo.writelines(ran_str)
         print(ran_str)
-        time.sleep(0.5)
-    while True:     # Once all requested words have been generated, will ask if the user wants to make more
-        I2 = input('Would you like to generate more? (y/n): ').lower()
-        if I2 == 'n':
-            print("Okay, closing.\n")
-            input("[Press Enter to exit]")
-            var = 1
-            break
-        elif I2 == 'y':
-            break
+
+
+if __name__ == "__main__":
+    fo = open('wordlist.txt', 'a')
+
+    print("Welcome to my Python wordlist generator! Made with <3 by WilliamAfton-codes")
+
+    while True:
+        threadnum = input("How many threads would you like to use?: ")
+        if threadnum.isdigit():
+            if int(threadnum) > 0:
+                break
+            else:
+                print("Please enter a number larger than 0!")
         else:
-            print("Error, please type either y or n to make a selection")
+            print("Please input a valid integer!")
+
+    strlength = int(input("How long would you like each string to be?: "))
+
+    strnum = int(input("How many strings do you want to generate (Each thread will generate the selected amount)?: "))
+
+    for i in range(int(threadnum)):
+        x = threading.Thread(target=process)
+        x.start()
+        x.join()
+
+    input("[Press Enter to exit.]")
